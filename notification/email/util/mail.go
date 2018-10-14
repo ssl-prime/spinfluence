2a3af3691fc86538cfd4a566ef9e0eb09073ca6e
ssl-prime <skcse03@gmail.com>
2018-10-14 20:06:31 +0530

    api key

--- notification/email/util/mail.go
+++ notification/email/util/mail.go
@@ -15,7 +15,7 @@ func SendMail(token string) {
 	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>" + token
 	subject := "reset password"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient("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")
+	client := sendgrid.NewSendClient("API-key of Send grid")
 	if response, err := client.Send(message); err == nil {
 		fmt.Println(response)
 	}
